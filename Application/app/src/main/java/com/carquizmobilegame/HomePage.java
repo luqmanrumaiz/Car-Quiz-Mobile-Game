f24505447f1d387f0cfeb2c2d1b5ee56af6b6da2
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-03-01 23:00:30 +0530

    Added Navigation for the Identify Car Make Page
    Created an Intent to the Identify Make Page for the respective Button and made a Toolbar in the Identify Make Page no navigate back to the Home Page

--- Application/app/src/main/java/com/carquizmobilegame/HomePage.java
+++ Application/app/src/main/java/com/carquizmobilegame/HomePage.java
@@ -2,7 +2,9 @@ package com.carquizmobilegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class HomePage extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
     }
+
+    public void switchToIdentifyMake(View view)
+    {
+        Intent intent = new Intent(this, IdentifyCarMake.class);
+        startActivity(intent);
+    }
+
+
 }
--- Application/app/src/main/java/com/carquizmobilegame/IdentifyCarMake.java
+++ Application/app/src/main/java/com/carquizmobilegame/IdentifyCarMake.java
@@ -1,15 +1,36 @@
 package com.carquizmobilegame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class IdentifyCarMake extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify_car_make);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle("");
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
